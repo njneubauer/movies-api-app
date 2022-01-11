91c01e26351c7c43ebb7c025ce81582b89680707
njneubauer <njneubauer@gmail.com>
2022-01-11 16:43:45 -0600

    deleted commented out code for cors

--- index.js
+++ index.js
@@ -24,18 +24,6 @@ const cors = require('cors');
 
 app.use(cors());
 
-// const allowedOrigins = ['http://localhost:8080'];
-// app.use(cors({
-//     origin: (origin, callback)=>{
-//         if(!origin) return callback(null, true);
-//         if(allowedOrigins.indexOf(orgin) === -1){
-//             let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-//             return callback(new Error(message), false);
-//         }
-//         return callback(null, true)
-//     }
-// }));
-
 // import auth.js file and pass express() to it.
 let auth = require('./auth')(app);
 
