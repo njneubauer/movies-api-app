e7b11870108eb69668f5a8012748fbac2f05c34f
njneubauer <njneubauer@gmail.com>
2022-01-28 20:18:57 -0600

    added new cors policy

--- index.js
+++ index.js
@@ -21,7 +21,18 @@ app.use(morgan('common'));
 
 // import CORS and define accepted origins
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://nickflixmovieinfo.netlify.app/'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // import auth.js file and pass express() to it.
 let auth = require('./auth')(app);
