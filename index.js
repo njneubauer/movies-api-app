6a96788fea6b85299c2dc57ffffde601524b189d
njneubauer <njneubauer@gmail.com>
2022-01-30 23:19:47 -0600

    added birthday to DELETE route /:username/favorites/delete/:movieTitle

--- index.js
+++ index.js
@@ -473,6 +473,7 @@ app.delete('/:username/favorites/delete/:movieTitle', passport.authenticate('jwt
                                     _id: 1,
                                     username: 1,
                                     email: 1,
+                                    birthday: 1,
                                     favoriteMovies: 1,
                                     "favoriteMoviesInfo": {
                                         _id: 1,
