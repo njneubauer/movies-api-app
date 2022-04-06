2bdaad0325155122c2dff7d54e85009117624d14
njneubauer <njneubauer@gmail.com>
2022-04-06 11:37:10 -0500

    edited /registration validation logic message for email

--- index.js
+++ index.js
@@ -254,7 +254,7 @@ app.post('/registration',
     check('username', 'Username is required').isLength({min: 5}),
     check('username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
     check('password', 'Password is required').not().isEmpty(),
-    check('email', 'Email does not appear to be valid').isEmail()
+    check('email', 'Email format is not valid').isEmail()
   ], (req, res) => {
 
   // check the validation object for errors
